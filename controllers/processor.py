c364087ed3d53ee46a6b06fba9066a8b958974b7
jackelyn1397 <jackelyn1397@gmail.com>
2015-02-08 10:04:46 -0800

    Update processor.py

--- controllers/processor.py
+++ controllers/processor.py
@@ -40,14 +40,16 @@ def process(reviews, keywords):
 
                     if(i - startIndex > 120):
                         startIndex = i - 120
-                    #looks a space to indicate the end of a word
-                    while review[startIndex] != “ “
-                       startIndex += 1
+                    #looks for a space to indicate the end of a word
+                        while review[startIndex] != “ “:
+                            startIndex += 1
 
                         
                     #print "start: " + str(startIndex)
                     if (endIndex - i > 120):
                         endIndex = i + 120
+                        while review[endIndex] != “ “:
+                            endIndex -= 1
                     #print "end: " + str(endIndex);
 
                     phrases.append(("..." + review[startIndex:endIndex] + "...").decode('utf-8'))
