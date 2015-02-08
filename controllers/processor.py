bd60736f97add740325add333e9d1bd43ea316d8
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-02-07 16:34:59 -0800

    processor module created

---
+++ controllers/processor.py
@@ -0,0 +1,5 @@
+# @param    reviews        list of string reviews to process
+# @param    keywords        list of keywords to search for in each review
+# @return   processedList      list of processed strings that contain the keywords
+def process(reviews, keywords):
+
